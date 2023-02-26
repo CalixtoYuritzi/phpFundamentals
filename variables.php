3771ec48d17eaa326db56ef02a8f9c28c8c1e00d
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-02-25 22:10:57 -0600

    Create variables.php

---
+++ variables.php
@@ -0,0 +1,27 @@
+<?php
+$nombre = "Juan";
+$edad = 20;
+$altura = 1.72;
+$soltero = true;
+
+
+var_dump($nombre); //con este método sabemos el tipo de dato que se utiliza
+echo '<br>';
+echo $nombre;
+echo '<br>';
+
+var_dump($edad);
+echo '<br>';
+echo $edad;
+echo '<br>';
+
+var_dump($altura);
+echo '<br>';
+echo $altura;
+echo '<br>';
+
+var_dump($soltero);
+echo '<br>';
+echo $soltero;
+echo '<br>';
+?>
