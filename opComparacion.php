a5ab9edaedf31120d1a58eda47c2dcbae4fb60f3
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-02-26 14:34:11 -0600

    Create opComparacion.php

---
+++ opComparacion.php
@@ -0,0 +1,87 @@
+<?php
+/*
+//COMPARACION 1
+$a = 10;
+$b = 20;
+
+echo $a == $b; //False (No muestra nada en pantalla)
+echo '<br>';
+
+echo $a === $b; //False (No muestra nada en pantalla)
+echo '<br>';
+
+//Si lo cambio a 10
+$a = 10;
+$b = 10;
+echo $a == $b; //Son iguales (True o 1)
+echo $a === $b; //Son identicos (True o 1) si lo cambiamos a string
+$a = 10;
+$b = "10";
+echo $a == $b; //Son iguales (True o 0)
+echo $a === $b;//Pero no son identicos (False o 0)
+*/
+
+
+/*
+//COMPARACION 2
+$a = 10;
+$b = 20;
+
+echo $a != $b; //Es distinto (True o 1)
+echo '<br>';
+
+echo $a !== $b; //No son identicos o iguales (True o 1)
+echo '<br>';
+
+//Si lo cambio por un string
+$a = 10;
+$b = "10";
+
+echo $a != $b; //Es distinto (True o 1)
+echo '<br>';
+
+echo $a !== $b; //Son iguales pero no identicos (True o 1)
+echo '<br>';
+*/
+
+
+/*
+//COMPARACION 2
+$a = 10;
+$b = 20;
+echo $a > $b; //a mayor que b (falso o 0)
+echo '<br>';
+
+echo $a >= $b; //a mayor o igual que b (falso o 0)
+echo '<br>';
+
+//Si cambio a = 30, esto cambia
+$a = 30;
+$b = 20;
+echo $a > $b; //a mayor que b (true o 1)
+echo '<br>';
+
+echo $a >= $b; //a mayor o igual que b (true o 1)
+echo '<br>';
+*/
+
+/*
+//COMPARACION 3
+$a = 30;
+$b = 20;
+echo $a < $b; //a menor que b (falso o 0)
+echo '<br>';
+
+echo $a <= $b; //a menor o igual que b (falso o 0)
+echo '<br>';*/
+
+//Si lo cambio a su valor inicial
+$a = 10;
+$b = 20;
+echo $a < $b; //a menor que b (true o 1)
+echo '<br>';
+
+echo $a <= $b; //a menor o igual que b (true o 1)
+echo '<br>';
+
+?>
