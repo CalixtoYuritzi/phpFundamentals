7bd0f6809954b4c6f6319c5ad80c07f74b3ca07b
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-02-26 14:03:06 -0600

    Create opAsignacion.php

---
+++ opAsignacion.php
@@ -0,0 +1,24 @@
+<?php
+$x = 4.5;
+$y = 5;
+
+//Suma en asignación
+$x += $y;
+echo $x . '<br>';
+
+//Resta en asignación
+$x -= $y;
+echo $x . '<br>';
+
+//Multiplicación en asignación
+$x *= $y;
+echo $x . '<br>';
+
+//División en asignación
+$x /= $y;
+echo $x . '<br>';
+
+//Módulo en asignación
+$x %= $y;
+echo $x . '<br>';
+?>
