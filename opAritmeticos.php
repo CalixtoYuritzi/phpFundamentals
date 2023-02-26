9aad6d4066c886287abfdc03c2fe5e2dc56c2137
CalixtoYuritzi <89166721+CalixtoYuritzi@users.noreply.github.com>
2023-02-25 22:28:25 -0600

    Create opAritmeticos.php

---
+++ opAritmeticos.php
@@ -0,0 +1,20 @@
+<?php
+//Definir variable número
+$numero1 = 5;
+$numero2 = 2;
+
+//Suma
+echo $numero1 + $numero2 . '<br>';
+
+//Resta
+echo $numero1 - $numero2 . '<br>';
+
+//Multiplicación
+echo $numero1 * $numero2 . '<br>';
+
+//División
+echo $numero1 / $numero2 . '<br>';
+
+//Módulo
+echo $numero1 % $numero2 . '<br>';
+?>
